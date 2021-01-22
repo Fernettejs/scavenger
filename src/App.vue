db82a0793e0770853379bade39431077d233868d
Joe <fernettejs@gmail.com>
2021-01-22 06:40:57 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ButtonStar />
+</template>
+
+<script>
+import ButtonStar from './components/ButtonStar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ButtonStar
+  }
+}
+</script>
+
+<style>
+* {
+ box-sizing: border-box;
+ margin: 0;
+ padding: 0;
+}
+ 
+body {
+ font-family: Arial, Helvetica, sans-serif;
+ line-height: 1.4;
+}
+
+</style>
---
+++ src/components/ButtonStar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="button-star">
+    <button @click="showStar()">Big Red Button</button>
+    <img id="img" alt="star" src="../assets/star.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonStar',
+  props: {
+  },
+  methods: {
+    showStar() {
+      const img = document.getElementById('img');
+      img.classList.add('visible')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  height: 200px;
+  width: 200px;
+  border-radius: 100px;
+  color: white;
+  background: red;
+  position: absolute;
+  left: 50px;
+  bottom: 20px;
+  cursor: pointer;
+}
+
+img {
+  visibility: hidden;
+}
+.visible {
+  visibility: visible;
+}
+
+</style>
